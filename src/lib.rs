6ff319d5286439949bbac751dc3321a158fe3e59
Derek Panger <12522587+DPanger13@users.noreply.github.com>
2024-01-02 20:10:19 -0600

    Set up UniFFI (#5)

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
+uniffi::setup_scaffolding!();
+
+#[derive(uniffi::Record)]
 pub struct Home {
     pub total: i32,
     pub percentage: u8,
 }
 
+#[uniffi::export]
 pub fn home() -> Home {
     Home {
         total: 8,
