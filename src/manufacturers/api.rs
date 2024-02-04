ab90bba42bbc838a3b886867d70fc30dcf91c0bf
Derek Panger <12522587+DPanger13@users.noreply.github.com>
2024-02-04 17:46:35 -0600

    Implement Manufacturers API

---
+++ src/manufacturers/api.rs
@@ -0,0 +1,22 @@
+use serde::Deserialize;
+use crate::api::VEHICLES_API;
+
+#[derive(Deserialize)]
+pub struct ManufacturersResponse {
+    #[serde(rename(deserialize = "Results"))]
+    pub manufacturers: Vec<ManufacturerResponse>,
+}
+
+#[derive(Deserialize)]
+pub struct ManufacturerResponse {
+    #[serde(rename(deserialize = "Mfr_ID"))]
+    pub id: String,
+
+    #[serde(rename(deserialize = "Mfr_CommonName"))]
+    pub name: String,
+}
+
+pub fn fetch_manufacturers() -> reqwest::Result<ManufacturersResponse> {
+    let full_url = VEHICLES_API.to_owned() + "/vehicles/GetAllManufacturers?format=json";
+    reqwest::blocking::get(full_url)?.json::<ManufacturersResponse>()
+}
