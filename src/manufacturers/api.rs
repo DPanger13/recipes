e6af3cfe3777416ac40f4ae17c23916c35d3a43a
Derek Panger <12522587+DPanger13@users.noreply.github.com>
2025-02-07 22:54:51 -0600

    Add function for searching manufacturers

--- src/manufacturers/api.rs
+++ src/manufacturers/api.rs
@@ -20,3 +20,8 @@ pub fn fetch_manufacturers() -> reqwest::Result<ManufacturersResponse> {
     let full_url = VEHICLES_API.to_owned() + "/vehicles/GetAllManufacturers?format=json";
     reqwest::blocking::get(full_url)?.json::<ManufacturersResponse>()
 }
+
+pub fn fetch_manufacturers_by_name(name: String) -> reqwest::Result<ManufacturersResponse> {
+    let full_url = VEHICLES_API.to_owned() + "/vehicles/GetManufacturerDetails/" + &name;
+    reqwest::blocking::get(full_url)?.json::<ManufacturersResponse>()
+}
